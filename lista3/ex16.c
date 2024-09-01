37567f6443434f1fdaf2def407ce311be10e92ae
Ana Giulia Aguiar <imanagiulia@gmail.com>
2024-09-01 13:11:55 -0300

    finalização exercício 16

--- lista3/ex16.c
+++ lista3/ex16.c
@@ -6,6 +6,8 @@ int main(){
 
     int qtdMercadoria;
     float valorMerdadoria[qtdMercadoria];
+    float soma = 0, total;
+
 
     printf("Digite o número total de mercadorias no estoque: ");
     scanf("%d", &qtdMercadoria);
@@ -13,5 +15,13 @@ int main(){
     for ( int i = 0; i < qtdMercadoria; i++){
         printf("Digite o valor da mercadoria %d: ", i+1);
         scanf("%f", &valorMerdadoria[i]);
+
+        soma += valorMerdadoria[i];
+        total = qtdMercadoria*valorMerdadoria[i];
     }
+
+    float media = soma/qtdMercadoria;
+
+    printf("O valor total em estoque é de R$%.2f \n", total);
+    printf("A média do valor das mercadoria é R$%.2f", media);
 }
